0b3ccbfb366a030ebaa2548f1e8f98bca5a62422
ericq-mparticle <ericq@mparticle.com>
2016-07-14 12:54:24 -0400

    Added alert

--- Test Project/ViewController.swift
+++ Test Project/ViewController.swift
@@ -12,12 +12,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        showAlert()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func showAlert() {
+        let alert = UIAlertController(title: "Annoying Alert", message: "Right when you launch the app", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Go Away", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
     }
 
 
